53e405bfd1534a24d025646c555066bfe55c1a77
zengrg <bijeng695@gmail.com>
2023-08-20 17:41:32 +0545

    typo fixed

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,7 +4,7 @@ import codings from '../../assets/lottie/coding.json';
 import { ThemeContext } from '../../contexts/theme-context';
 import { aboutData } from '../../data/about-data';
 import styles from '../../styles/about.module.css';
-import AnimationLottie from '../Animation';
+import AnimationLottie from '../animation';
 
 
 function About() {
