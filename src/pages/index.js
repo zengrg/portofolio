a9f8983e6cd26345f2cd985815fe62043a4bc395
zengrg <bijeng695@gmail.com>
2023-08-22 12:07:10 +0545

    c

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import {
 import BackToTop from '../components/back-to-top/back-to-top';
 import ChangeTheme from '../components/change-theme/change-theme';
 
-function HomePage({ blogs }) {
+function HomePage() {
 
   return (
     <>
